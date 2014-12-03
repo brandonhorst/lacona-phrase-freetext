ce8c5bea9384becd5750d95ca4b5f0564a8b78e5
Brandon Horst <brandonhorst@gmail.com>
2014-12-03 13:30:19 -0500

    Implement default, fix regex

--- test/test.js
+++ test/test.js
@@ -17,7 +17,6 @@ describe('lacona-phrase-freetext', function() {
 		var schema = {
 			phrases: [{
 				name: 'test',
-				langs: ['default'],
 				root: {
 					type: 'freetext',
 					id: 'test'
@@ -44,123 +43,97 @@ describe('lacona-phrase-freetext', function() {
 		.parse('anything');
 	});
 
-	// it('handles an standalone, regex freetext (accepted)', function(done) {
-	// 	var schema = {
-	// 		root: {
-	// 			type: 'freetext',
-	// 			id: 'test'
-	// 			regex =
-	// 		},
-	// 		sentence: true
-	// 	};
+	it('handles a regex freetext', function(done) {
+		var schema = {
+			phrases: [{
+				name: 'test',
+				root: {
+					type: 'freetext',
+					regex: /anything/,
+					id: 'test'
+				}
+			}],
+			dependencies: [freetext]
+		};
 
-	// 	var onData = chai.spy(function() {
-	// 		expect(data.match[0].string).to.equal('result');
-	// 		expect(data.result.test).to.equal('result');
-	// 	});
 
-	// 	var onEnd = function() {
-	// 		expect(onData).to.have.been.called.once;
-	// 		done();
-	// 	};
+		var onData = sinon.spy(function(data) {
+			expect(data.match[0].string).to.equal('anything');
+			expect(data.result.test).to.equal('anything');
+		});
 
-	// 	new Parser()
-	// 	.understand(testCase.).on('data', onData)
-	// 	.on('end', onEnd)
-	// 	.parse(testCase.input);
-	// });
-	// 		}, {
-	// 			input: 'anything',
-	// 			desc: 'regex (accepted)',
-	// 			schema: {
-	// 				root: {
-	// 					type: 'freetext',
-	// 					regex: /anything/
-	// 				},
-	// 				run: ''
-	// 			},
-	// 			result: 'anything',
-	// 			matches: 1
-	// 		}, {
-	// 			input: 'anything',
-	// 			desc: 'regex (rejected)',
-	// 			schema: {
-	// 				root: {
-	// 					type: 'freetext',
-	// 					regex: /nothing/
-	// 				},
-	// 				run: ''
-	// 			},
-	// 			matches: 0
-	// 		}, {
-	// 			input: 'anything',
-	// 			desc: 'string regex',
-	// 			schema: {
-	// 				root: {
-	// 					type: 'freetext',
-	// 					regex: 'anything'
-	// 				},
-	// 				run: ''
-	// 			},
-	// 			result: 'anything',
-	// 			matches: 1
-	// 		}
-	// 	];
+		var onEnd = function() {
+			expect(onData).to.have.been.called.once;
+			done();
+		};
 
-	// 	async.each(testCases, function(testCase, done) {
-	// 		var dataCalled;
-	// 		dataCalled = chai.spy();
-	// 		return new Parser().understand(testCase.schema).on('data', function(data) {
-	// 			dataCalled();
-	// 			expect(data, testCase.desc).to.exist;
-	// 			expect(data.match, testCase.desc).to.exist;
-	// 			expect(data.match, testCase.desc).to.have.length(1);
-	// 			return expect(data.match[0].string, testCase.desc).to.equal(testCase.result);
-	// 		}).on('end', function() {
-	// 			expect(dataCalled, testCase.desc).to.have.been.called.exactly(testCase.matches);
-	// 			return done();
-	// 		}).parse(testCase.input);
-	// 	}, done);
-	// });
+		parser
+		.understand(schema)
+		.on('data', onData)
+		.on('end', onEnd)
+		.parse('anything');
+	});
 
-	// it('handles a freetext in a sequence', function(done) {
-	// 	var dataCalled, testCase;
-	// 	testCase = {
-	// 		input: 'anything',
-	// 		desc: 'anything',
-	// 		schema: {
-	// 			root: {
-	// 				type: 'sequence',
-	// 				separator: null,
-	// 				children: [
-	// 					{
-	// 						type: 'freetext',
-	// 						regex: /any/,
-	// 						id: 'test'
-	// 					}, 'thing'
-	// 				]
-	// 			},
-	// 			run: ''
-	// 		},
-	// 		match: ['any', 'thing'],
-	// 		result: {
-	// 			test: 'any'
-	// 		},
-	// 		matches: 1
-	// 	};
-	// 	dataCalled = chai.spy();
-	// 	return new Parser().understand(testCase.schema).on('data', function(data) {
-	// 		var i, _i, _ref;
-	// 		dataCalled();
-	// 		expect(data, testCase.desc).to.exist;
-	// 		expect(data.match, testCase.desc).to.exist;
-	// 		for (i = _i = 0, _ref = testCase.match.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
-	// 			expect(data.match[i].string, testCase.desc).to.equal(testCase.match[i]);
-	// 		}
-	// 		return expect(data.result, testCase.desc).to.deep.equal(testCase.result);
-	// 	}).on('end', function() {
-	// 		expect(dataCalled, testCase.desc).to.have.been.called.exactly(testCase.matches);
-	// 		return done();
-	// 	}).parse(testCase.input);
-	// });
+	it('handles a plain-text regex freetext', function(done) {
+		var schema = {
+			phrases: [{
+				name: 'test',
+				root: {
+					type: 'freetext',
+					regex: 'anything',
+					id: 'test'
+				}
+			}],
+			dependencies: [freetext]
+		};
+
+
+		var onData = sinon.spy(function(data) {
+			expect(data.match[0].string).to.equal('anything');
+			expect(data.result.test).to.equal('anything');
+		});
+
+		var onEnd = function() {
+			expect(onData).to.have.been.called.once;
+			done();
+		};
+
+		parser
+		.understand(schema)
+		.on('data', onData)
+		.on('end', onEnd)
+		.parse('anything');
+	});
+
+	it('suggests a default', function(done) {
+		var schema = {
+			phrases: [{
+				name: 'test',
+				root: {
+					type: 'freetext',
+					default: 'whatever',
+					id: 'test'
+				}
+			}],
+			dependencies: [freetext]
+		};
+
+
+		var onData = sinon.spy(function(data) {
+			expect(data.suggestion.words[0].string).to.equal('whatever');
+			expect(data.result.test).to.equal('whatever');
+		});
+
+		var onEnd = function() {
+			expect(onData).to.have.been.called.once;
+			done();
+		};
+
+		parser
+		.understand(schema)
+		.on('data', onData)
+		.on('end', onEnd)
+		.parse('');
+	});
+
 });
